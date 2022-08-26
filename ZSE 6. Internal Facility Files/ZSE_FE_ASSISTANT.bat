c6613a611e2b2fd36650ebf6e42412afe0ba0b2f
Teddy <63885870+cessnahat@users.noreply.github.com>
2022-08-26 09:17:31 -0700

    Add CYVR and CYYJ runway outlines
    Implements #2

--- ZSE 6. Internal Facility Files/ZSE_FE_ASSISTANT.bat
+++ ZSE 6. Internal Facility Files/ZSE_FE_ASSISTANT.bat
@@ -12,7 +12,7 @@ IF EXIST %userprofile%\Dropbox (
 
 :: ZSE 6. Internal Facility Files location
 IF EXIST "%userprofile%\Documents\Facility-Files\ZSE 6. Internal Facility Files" (
-	SET IFF_PATH=%userprofile%\Facility-Files\ZSE 6. Internal Facility Files
+	SET IFF_PATH=%userprofile%\Documents\Facility-Files\ZSE 6. Internal Facility Files
 ) ELSE IF %USERNAME%==mgg4b (
 	SET IFF_PATH=C:\Users\mgg4b\OneDrive\Documents\GitHub\Facility-Files\ZSE 6. Internal Facility Files
 ) ELSE (

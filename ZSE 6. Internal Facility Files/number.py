82f0208306d7e14129d5eb6e6a8f89a28f02aa59
GunGuyAlex <100453092+GunGuyAlex@users.noreply.github.com>
2025-01-11 15:31:57 -0500

    Delete ZSE 6. Internal Facility Files/number.py

--- ZSE 6. Internal Facility Files/number.py
+++
@@ -1,9 +0,0 @@
-# Use to (re)number files (e.g. 100_APD.xml, 110_VORs.xml, 120_MVA.xml, ...)
-
-import os
-n = 100
-extension = input("File extension: .")
-for entry in os.listdir():
-	if entry.endswith("." + extension):
-		os.rename(entry, str(n) + entry[entry.index("_"):])
-		n += 10

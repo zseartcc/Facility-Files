6edaa43b534cc5166dd90465181d73e59bbbdb7e
Matthew Armstrong <matthewarmstrong862@gmail.com>
2026-03-27 20:26:03 +0000

    update bat script to auto append airac cycle

--- ZSE 6. Internal Facility Files/EDIT ALIAS/Merger.bat
+++ ZSE 6. Internal Facility Files/EDIT ALIAS/Merger.bat
@@ -1,1 +1,6 @@
-copy *.txt ..\ZSE_ALIASES.txt
+@echo off
+
+for /f %%A in ('powershell -NoProfile -Command ^
+  "(Invoke-RestMethod 'https://airac.net/api/v1/airac/current').data.cycle"') do set AIRAC=%%A
+
+copy *.txt "..\ZSE_ALIASES_%AIRAC%.txt"
